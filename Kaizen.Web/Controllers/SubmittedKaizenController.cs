3fe69566cb6905080f7ddc7645b5b0f388ce2878
Manas Kumar Lenka <manaskumar.l@canopusgbs.com>
2024-06-20 18:55:52 +0530

    Reset Button , Page Refresh After Popup on Delete Button , Submitted Kaizen Page Added

---
+++ Kaizen.Web/Controllers/SubmittedKaizenController.cs
@@ -0,0 +1,34 @@
+﻿using Kaizen.Business.Interface;
+using Kaizen.Data.Constant;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Xml.Linq;
+using Kaizen.Web;
+using System.Reflection.Metadata;
+using Kaizen.Models.ViewUserModel;
+using Kaizen.Models.AdminModel;
+using Kaizen.Business.Worker;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Kaizen.Web.Controllers
+{
+    public class SubmittedKaizenController : Controller
+    {
+        private readonly IDomain _domainWorker;
+        private readonly IDepartment   _departmentWorker;
+		private readonly IWebHostEnvironment _environment;
+        public SubmittedKaizenController(IDomain domainWorker,IDepartment departmentWorker)
+        {
+            _domainWorker = domainWorker;
+            _departmentWorker = departmentWorker;
+        }
+
+        public IActionResult ViewKaizen()
+        {
+            return View();
+        }
+    }
+}
