3a7713d81c3312239a8e6c2251b42f8ef3fa3e2b
chaitusangam <chaithanya.s@canopusgbs.com>
2024-06-20 18:34:35 +0530

    26-06-2024 changes in NewKaizen

---
+++ Kaizen.Web/Controllers/CreateKaizenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kaizen.Web.Controllers
+{
+    public class CreateKaizenController : Controller
+    {
+        public IActionResult NewKaizen()
+        {
+            return View();
+        }
+    }
+}
