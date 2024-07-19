6f22b7e387d374c8158adbeca3c593cd75932099
chaitusangam <chaithanya.s@canopusgbs.com>
2024-07-19 12:48:15 +0530

    New Changes in ReportsDownload(19-07-2024)

--- Kaizen.Business/Interface/IReport.cs
+++ Kaizen.Business/Interface/IReport.cs
@@ -5,11 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace Kaizen.Business.Interface
 {
     public interface IReport
     {
         public DataTable GetKaizenform(KaizenReportModel model);
+
+        public byte[] ExportDataTableToExcel(DataTable dataTable);
+        
     }
 }
