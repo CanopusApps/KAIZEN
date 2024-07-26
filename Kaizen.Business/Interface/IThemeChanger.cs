39dfa0e268b57b3f811a2244461b49318f2fbb4b
SMR5M7 <sudhan.manuel@canopusgbs.com>
2024-07-26 15:44:36 +0530

    Theme changer- Initial push

---
+++ Kaizen.Business/Interface/IThemeChanger.cs
@@ -0,0 +1,20 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using Kaizen.Models.AdminModel;
+using Kaizen.Models.Theme;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizen.Business.Interface
+{
+    public interface IThemeChanger
+    {
+        public bool AddTheme(ThemeModel model);
+
+        List<ThemeModel> RetrieveTheme();
+    }
+}
---
+++ Kaizen.Business/Worker/ThemeWorker.cs
@@ -0,0 +1,48 @@
+﻿using Kaizen.Business.Interface;
+using Kaizen.Data.DataServices.Interfaces;
+
+using Kaizen.Data.DataServices;
+using Kaizen.Models.Theme;
+using Kaizen.Models.WinnersList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Kaizen.Business.Worker    
+{
+    public class ThemeWorker : IThemeChanger
+    {
+        public readonly IThemeRepository _themeRepository;
+        public ThemeWorker(IThemeRepository themeRepository)
+        {
+            _themeRepository = themeRepository;
+        }
+        public bool AddTheme(ThemeModel model)
+        {
+            return _themeRepository.AddTheme(model);
+        }
+        public List<ThemeModel> RetrieveTheme()
+        {
+            DataSet ds;
+            List<ThemeModel> RetrieveTheme = new List<ThemeModel>();
+            ds = _themeRepository.RetrieveTheme();
+            if (ds.Tables.Count > 0)
+            {
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    RetrieveTheme.Add(new ThemeModel   
+                    {
+                        ModifiedBy = dr["ModifiedBy"].ToString(),
+                        Theme = dr["Theme"].ToString()
+
+                    });
+                }
+            }
+            return RetrieveTheme;
+        }
+
+    }
+}
