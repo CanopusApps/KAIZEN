a998d5c77b0bc9f65924cd2ca22b51b2ed53b098
Gowtham Vallabhaneni <gowtham.v@Canopusgbs.com>
2024-09-30 11:02:12 +0530

    Winner List cleared date format bug

--- Kaizen.Business/Worker/WinnersListWorker.cs
+++ Kaizen.Business/Worker/WinnersListWorker.cs
@@ -2,6 +2,7 @@
 using Kaizen.Business.Interface;
 using Kaizen.Data.DataServices;
 using System.Data;
+using System.Globalization;
 
 namespace Kaizen.Business.Worker
 {
@@ -29,38 +30,62 @@ namespace Kaizen.Business.Worker
         {
             return _winnersListRepository.ToggleStatus(model);
         }
+
         public List<WinnersListModel> GetWinners()
         {
             DataSet ds;
             List<WinnersListModel> winnModels = new List<WinnersListModel>();
             ds = _winnersListRepository.GetWinners();
-             
+
             if (ds.Tables.Count > 0)
             {
                 foreach (DataRow dr in ds.Tables[0].Rows)
                 {
                     string filePath = dr["FileName"].ToString();
                     string base64String = ConvertFileToBase64(filePath);
+
+                    string[] formats = { "MM/dd/yyyy", "MMM/dd/yyyy", "dd/MM/yyyy", "yyyy-MM-dd", "MM-dd-yyyy", "dd-MM-yyyy", "dd MMM yyyy" };
+
+                    DateTime parsedStartDate = ParseDate(dr["StartDate"].ToString(), formats);
+                    DateTime parsedEndDate = ParseDate(dr["EndDate"].ToString(), formats);
+
                     winnModels.Add(new WinnersListModel
                     {
-                        Id= Guid.Parse(dr["ID"].ToString()),
-                        EmpID= Convert.ToInt32(dr["UserID"]),
-                        EmpName= dr["WinnerName"].ToString(),
-                        Category= dr["Category"].ToString(),
-                        DomainName= dr["DomainName"].ToString(),
+                        Id = Guid.Parse(dr["ID"].ToString()),
+                        EmpID = Convert.ToInt32(dr["UserID"]),
+                        EmpName = dr["WinnerName"].ToString(),
+                        Category = dr["Category"].ToString(),
+                        DomainName = dr["DomainName"].ToString(),
                         DepartmentName = dr["DepartmentName"].ToString(),
-                        StartDate= Convert.ToDateTime(dr["StartDate"]),
-                        EndDate = Convert.ToDateTime(dr["EndDate"]),
+                        StartDate = parsedStartDate,
+                        EndDate = parsedEndDate,
                         Status = dr["Status"].ToString(),
-                        Winnerimgfilename= base64String,
-                        winnerimagefilepath= filePath
-
-
+                        Winnerimgfilename = base64String,
+                        winnerimagefilepath = filePath
                     });
                 }
             }
             return winnModels;
         }
+
+        private DateTime ParseDate(string dateString, string[] formats)
+        {
+            DateTime parsedDate;
+
+            if (DateTime.TryParseExact(dateString, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate))
+            {
+                return parsedDate;
+            }
+
+            if (DateTime.TryParse(dateString, out parsedDate))
+            {
+                return parsedDate;
+            }
+
+            return DateTime.MinValue;
+        }
+
+
         private string ConvertFileToBase64(string filePath)
         {
             if (File.Exists(filePath))
